eef89c1a0fdcc7b844e18196fdf337f11724d11e
cospamental <cospa18@gmail.com>
2018-04-17 11:05:30 +0300

    added product service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductsService } from './services/products.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private prodService: ProductsService){}
+  
 }
