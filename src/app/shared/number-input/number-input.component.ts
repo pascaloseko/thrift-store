01f6ac291a2ed835a8ff8148acaceb35691c2085
cospamental <cospa18@gmail.com>
2018-04-17 09:47:53 +0300

    component that shows the number inputs

---
+++ src/app/shared/number-input/number-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-number-input',
+  templateUrl: './number-input.component.html',
+  styleUrls: ['./number-input.component.css']
+})
+export class NumberInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
