c515ad68078e743c975a8c98fe33b5958b3fe8d4
cospamental <cospa18@gmail.com>
2018-04-17 11:08:43 +0300

    filter component details

--- src/app/products/filters/filters.component.ts
+++ src/app/products/filters/filters.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-filters',
@@ -6,10 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filters.component.css']
 })
 export class FiltersComponent implements OnInit {
+  filterBy: string;
+  filterToggle: boolean;
+  @ViewChild('searchInput') searchText: ElementRef;
+  layoutMode: boolean; // true for grid, false for list
 
-  constructor() { }
+  constructor(private prodService: ProductsService) { }
 
   ngOnInit() {
+    this.setFilterToggle();
+    this.filterBy = this.prodService.getFilter()
+    this.prodService.filterTypeEmitter.subscribe(
+      (filterValue: string) => {
+        this.filterBy = filterValue;
+      }
+    );
+    this.layoutMode = this.prodService.getLayout();
+    this.prodService.layoutModeEmitter.subscribe(
+      (layoutVal: boolean) => {
+        this.layoutMode = layoutVal;
+      }
+    );
+  }
+
+  setLayout(layoutVal: boolean) {
+    this.prodService.setLayout(layoutVal);
+  }
+
+  setFilterToggle() {
+    this.filterToggle = window.outerWidth > 768 ? true : false;
+  }
+
+  searchFilter() {
+    this.prodService.searchFilter(this.searchText.nativeElement.value);
+  }
+
+  setFilter(filterValue: string) {
+    this.prodService.setFilter(filterValue);
+  }
+
+
+  resetFilters() {
+    this.prodService.setFilter('all');
+    this.searchText.nativeElement.value = '';
+    this.prodService.searchFilter('');
   }
 
 }
