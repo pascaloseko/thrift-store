25dc6808564572d901ec17bbd257ca482a1af6a7
cospamental <cospa18@gmail.com>
2018-04-17 11:09:33 +0300

    product card component details

--- src/app/products/product-card/product-card.component.ts
+++ src/app/products/product-card/product-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Product } from '../product.model';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-product-card',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent implements OnInit {
+  @Input() product: Product;
+  @Input() layoutMode: boolean;
 
-  constructor() { }
+  constructor(private prodService: ProductsService) { }
 
   ngOnInit() {
+  }
+
+  onAddToCart(product: Product) {
+    this.prodService.addToCart(product);
   }
 
 }
