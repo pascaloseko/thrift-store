1e765392744ca2690ad91f9faaafb1a86e317ccb
cospamental <cospa18@gmail.com>
2018-04-17 11:09:13 +0300

    single product component details

--- src/app/products/product/product.component.ts
+++ src/app/products/product/product.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastyNotificationsService } from './../../services/toasty-notifications.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Product } from '../product.model';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  id: string;
+  product: Product;
+  similarProduct: Product[];
+  isLoading = true;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private prodService: ProductsService
+  ) { }
 
   ngOnInit() {
+    this.initProductSingleView();
+  }
+
+  // helper fn to save repeating same code in init and doCheck hooks
+  initProductSingleView() {
+    this.id = this.route.snapshot.params['id'];
+    this.prodService.fetchSingleProductFromDB(this.id).subscribe(
+      product => {
+        this.product = product;
+        // this.getSimilarProducts(this.product.type, this.product.id);
+      },
+      err => console.error(err),
+      () => this.isLoading = false
+    );
+  }
+
+  addToCart(product: Product) {
+    this.prodService.addToCart(product);
   }
 
 }
