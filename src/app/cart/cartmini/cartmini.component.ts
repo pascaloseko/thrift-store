946164b0e75f00e0efae778b9dffe823576021e0
cospamental <cospa18@gmail.com>
2018-04-17 11:06:53 +0300

    mini cart component details

--- src/app/cart/cartmini/cartmini.component.ts
+++ src/app/cart/cartmini/cartmini.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { ProductsService } from '../../services/products.service';
+import { Product } from '../../products/product.model';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-cartmini',
   templateUrl: './cartmini.component.html',
   styleUrls: ['./cartmini.component.css']
 })
-export class CartminiComponent implements OnInit {
+export class CartminiComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  cartminiProducts: Product[];
+  cartminiTotal: number;
+  cartAdditionSubscription: Subscription;
+  cartTotalSubscription: Subscription;
+
+  constructor(private prodService: ProductsService) { }
 
   ngOnInit() {
+    this.cartminiProducts = this.prodService.getCartAddedProducts();
+    this.cartAdditionSubscription = this.prodService.cartAdditionEmitter.subscribe(
+      (products: Product[]) => {
+        this.cartminiProducts = products;
+      }
+    );
+
+    this.cartminiTotal = this.prodService.cartTotalEmitter.subscribe(
+      (cTotal: number) => {
+        this.cartminiTotal = cTotal;
+      }
+    );
   }
+
+  removeCartProduct(itemIndex: number) {
+    this.prodService.removeCartSingleItem(itemIndex);
+  }
+
+  emptyCart() {
+    this.prodService.emptyCart();
+  }
+
+
+
+  ngOnDestroy() {
+    this.cartAdditionSubscription.unsubscribe();
+    this.cartTotalSubscription.unsubscribe();
+  }
+
+
 
 }
